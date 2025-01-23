5642d24d1651ed47ea68cb1c3c7ebcb3f267a9da
DELL <adarsh.kulal01@gmail.com>
2025-01-23 13:30:54 +0530

    new repo

---
+++ src/constant/api_constants.py
@@ -0,0 +1,13 @@
+
+class APIconstants:
+    def base_url(self):
+        return "https://restful-booker.herokuapp.com"
+
+    def url_create_booking(self):
+        return "https://restful-booker.herokuapp.com/booking"
+
+    def url_create_token(self):
+        return "https://restful-booker.herokuapp.com/auth"
+
+
+
